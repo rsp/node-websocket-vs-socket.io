8d7b9eabe13ce158859e1e350fe9406102191d35
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-07-20 14:29:23 +0200

    update ws-vs-si.js

--- ws-vs-si.js
+++ ws-vs-si.js
@@ -8,34 +8,41 @@ var express = require('express');
 
 // WebSocket:
 var ws = {app: express(), pre: "websocket app: "};
+ws.log = m => console.error(ws.pre+m);
 ws.ws = require('express-ws')(ws.app);
 ws.app.get('/', (req, res) => {
-  console.error(ws.pre+'express connection');
+  ws.log('express connection - sending html');
   res.sendFile(__dirname + '/ws.html');
 });
 ws.app.ws('/', (s, req) => {
-  console.error('websocket connection');
+  ws.log('websocket connection');
   for (var t = 0; t < 3; t++)
-    setTimeout(() => s.send(ws.pre+'message from server'), 1000*t);
+    setTimeout(() => {
+      ws.log('sending message to client');
+      s.send(ws.pre+'message from server');
+    }, 1000*t);
 });
 ws.app.listen(3001, () =>
-  console.error(ws.pre+'listening on http://localhost:3001/'));
-console.error(ws.pre+'starting server');
+  ws.log('listening on http://localhost:3001/'));
+ws.log('starting server');
 
 // Socket.IO:
 var si = {app: express(), pre: "socket.io app: "};
+si.log = m => console.error(si.pre+m);
 si.http = require('http').Server(si.app);
 si.io = require('socket.io')(si.http);
 si.app.get('/', (req, res) => {
-  console.error(ws.pre+'express connection');
+  si.log('express connection - sending html');
   res.sendFile(__dirname + '/si.html');
 });
 si.io.on('connection', s => {
-  console.error('socket.io connection');
+  si.log('socket.io connection');
   for (var t = 0; t < 3; t++)
-    setTimeout(() =>
-      s.emit('message', si.pre+'message from server'), 1000*t);
+    setTimeout(() => {
+      si.log('sending message to client');
+      s.emit('message', si.pre+'message from server');
+    }, 1000*t);
 });
 si.http.listen(3002, () =>
-  console.error(si.pre+'listening on http://localhost:3002/'));
-console.error(si.pre+'starting server');
+  si.log('listening on http://localhost:3002/'));
+si.log('starting server');
